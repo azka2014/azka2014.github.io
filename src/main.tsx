00710684c960e3b3fc1acd6a88d9b3217734058d
[dyad] <git@dyad.sh>
2025-05-15 19:57:41 +0700

    Reverted all changes back to version 0fdaa4d8584b4102e6d5697420d72015cd24fb5e

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'; // Import React
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { InventoryProvider } from "./context/InventoryContext.tsx";
+import { InventoryProvider } from "./context/InventoryContext.tsx"; // Import the provider
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <InventoryProvider>
+  <React.StrictMode> {/* Keep StrictMode */}
+    <InventoryProvider> {/* Wrap App with InventoryProvider */}
       <App />
     </InventoryProvider>
   </React.StrictMode>
