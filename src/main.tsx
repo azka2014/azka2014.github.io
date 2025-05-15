1a887a1e7bb47b43606a731292c6eab961b1c7e6
[dyad] <git@dyad.sh>
2025-05-15 19:56:36 +0700

    Reverted all changes back to version 1859063037bb5b475bd7ea6d5aa10b6dfd2eca0a

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { DataProvider } from "./context/DataContext.tsx"; // Import DataProvider
 
-createRoot(document.getElementById("root")!).render(
-  <DataProvider> {/* Wrap App with DataProvider */}
-    <App />
-  </DataProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
