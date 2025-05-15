f40ab8288c102e67b28e3e7744abc39f3ca29f26
[dyad] <git@dyad.sh>
2025-05-15 19:56:10 +0700

    [dyad] wrote 3 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { DataProvider } from "./context/DataContext.tsx"; // Import DataProvider
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <DataProvider> {/* Wrap App with DataProvider */}
+    <App />
+  </DataProvider>
+);
