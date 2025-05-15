aea4a9162c595fdfa12ac1f0a7c529d037da6389
[dyad] <git@dyad.sh>
2025-05-15 19:57:48 +0700

    [dyad] Memperbaiki error 'React is not defined' - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import React from 'react'; // Import React
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { InventoryProvider } from "./context/InventoryContext.tsx"; // Import the provider
+import { InventoryProvider } from "./context/InventoryContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode> {/* Keep StrictMode */}
-    <InventoryProvider> {/* Wrap App with InventoryProvider */}
+  <React.StrictMode>
+    <InventoryProvider>
       <App />
     </InventoryProvider>
   </React.StrictMode>
